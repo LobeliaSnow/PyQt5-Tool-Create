f358da9e44b640921a24402546cbaa6cd09f215b
雪瑠璃 <lobelia.snow@gmail.com>
2018-08-19 04:07:51 +0900

    カメラ情報の取得を追加 TODO : テクスチャ情報取得部分に漏れがある(サンプラー等取っていない)ので、それをチートシートに沿って修正

--- DirectX11Test/Src/Plugin/gltf2_0Loader.py
+++ DirectX11Test/Src/Plugin/gltf2_0Loader.py
@@ -17,18 +17,18 @@ def DictInValue(dictionary,value_name):
         return dictionary[value_name]
     return None
 
-class Gltf2_0Node:
+class GLTF2_0Node:
     def __init__(self,tag):
         self.tag = tag
-class Gltf2_0MeshNode(Gltf2_0Node):
+class GLTF2_0MeshNode(GLTF2_0Node):
     def __init__(self,node_info):
-        super(Gltf2_0MeshNode,self).__init__("mesh")
+        super(GLTF2_0MeshNode,self).__init__("mesh")
         self.meshIndex = node_info["mesh"]
         #アニメーションの情報をこのノードが持っていなかったらNone
         self.skinIndex = DictInValue(node_info,"skin")
-class Gltf2_0TransformNode(Gltf2_0Node):
+class GLTF2_0TransformNode(GLTF2_0Node):
     def __init__(self,node_info):
-        super(Gltf2_0TransformNode,self).__init__("transform")
+        super(GLTF2_0TransformNode,self).__init__("transform")
         #親子関係周り
         self.children = DictInValue(node_info,"children")
         self.matrix = DictInValue(node_info,"matrix")
@@ -44,19 +44,38 @@ class Gltf2_0TransformNode(Gltf2_0Node):
             self.scale = DictInValue(node_info,"scale")
             if self.scale == None:
                 self.scale = [1.0,1.0,1.0]
-#現状使わない
-# class Gltf2_0CameraNode(Gltf2_0Node):
-#     def __init__(self,node_info):
-#         super(Gltf2_0CameraNode,self).__init__("camera")
-     
-class Gltf2_0Buffer:
+class GLTF2_0CameraNode(GLTF2_0Node):
+    def __init__(self,node_info):
+        super(GLTF2_0CameraNode,self).__init__("camera")
+        #カメラはトランスフォーム情報がカメラというだけなので
+        #トランスフォーム情報はトランスフォームのノードを所持します
+        self.transform = GLTF2_0TransformNode(node_info)
+
+#カメラ情報
+class GLTF2_0Camera:
+    def __init__(self,type_name):
+        self.typeName = type_name
+class GLTF2_0PerspectiveCamera(GLTF2_0Camera):
+    def __init__(self,aspect,fov,near,far):
+        self.aspect = aspect
+        self.fov = fov
+        self.near = near
+        self.far = far        
+class GLTF2_0OrthographicCamera(GLTF2_0Camera):
+    def __init__(self,xmag,ymag,near,far):
+        self.xmag = xmag
+        self.ymag = ymag
+        self.near = near
+        self.far = far  
+
+class GLTF2_0Buffer:
     def __init__(self,file_path,byte_length):
         self.file_path = file_path
         self.rawBuffer = open(file_path,"rb").read()
         self.byteLength = byte_length
         self.decodeData = {}
 
-class Gltf2_0MeshHeader:
+class GLTF2_0MeshHeader:
     def __init__(self,mesh_info):
         self.name = DictInValue(mesh_info,"name")
         if self.name == None:
@@ -81,14 +100,14 @@ class Gltf2_0MeshHeader:
         self.extension = DictInValue(primitives,"extension")
         self.extras = DictInValue(primitives,"extras")
 
-class gltf2_0BufferData:
+class GLTF2_0BufferData:
     def __init__(self,data,count,min_value,max_value):
         self.data = data
         self.count = count
         self.min = min_value
         self.max = max_value
 
-class Gltf2_0Loader:
+class GLTF2_0Loader:
     #外部用
     def __init__(self):
         self.header = None        
@@ -99,8 +118,7 @@ class Gltf2_0Loader:
         self.ParseHeader(file_path)
         #Gltf2.0用のローダーの為、それ以外は弾く
         if self.header["asset"]["version"] != "2.0":
-            print("エラー : gltf 2.0 以外のバージョンのファイルです")
-            return
+            raise Exception
         self.LoadBuffers()
         self.LoadImages()
         #ここからガッツリ情報が読みだされる
@@ -123,9 +141,10 @@ class Gltf2_0Loader:
     def LoadBuffers(self):
         self.buffers = []
         for buffer in self.header["buffers"]:
-            self.buffers += [Gltf2_0Buffer(self.relativePath + buffer["uri"],buffer["byteLength"])]
+            self.buffers += [GLTF2_0Buffer(self.relativePath + buffer["uri"],buffer["byteLength"])]
     #テクスチャを持っていない場合はself.images = Noneとなる
     def LoadImages(self):
+        #修正 チートシート通りにすること
         if "images" in self.header:
             self.images = []
             for image in self.header["images"]:
@@ -138,6 +157,8 @@ class Gltf2_0Loader:
         self.sceneNodes = self.header["scenes"][0]["nodes"]
         #self.nodesに必要なノードデータがそろう
         self.ParseNodes()
+        #カメラ情報
+        self.ParseCamera()
         #デフォルトのシーンがどれか
         self.defaultNode = DictInValue(self.header,"scene")
         if self.defaultNode == None:
@@ -149,24 +170,43 @@ class Gltf2_0Loader:
     #扱いやすい形式にノードをパースして変換
     def ParseNodes(self):
         self.nodes = []
-        #とりあえず、カメラは必要ないと思うので読み飛ばす
         for nodeInfo in self.header["nodes"]:
             node = None
             #このノードがメッシュを表すノードかどうか
             if "mesh" in nodeInfo:
-                node = Gltf2_0MeshNode(nodeInfo)
+                node = GLTF2_0MeshNode(nodeInfo)
             else:#トランスフォーム情報
-                #カメラ情報は読み飛ばす
                 if "camera" in nodeInfo:
-                    # node = Gltf2_0CameraNode(nodeInfo)
-                    continue
-                node = Gltf2_0TransformNode(nodeInfo)
+                    node = GLTF2_0CameraNode(nodeInfo)
+                else:
+                    node = GLTF2_0TransformNode(nodeInfo)
             self.nodes += [node]
+    #カメラ情報取得
+    def ParseCamera(self):
+        cameras = DictInValue(self.header,"cameras")
+        if cameras != None:
+            for camera in cameras:
+                self.cameras = []
+                typeName = camera["type"]
+                if typeName == "perspective":
+                    camera = camera["perspective"]
+                    far = DictInValue(self.header,"cameras")
+                    if far == None:
+                        far = 1000.0
+                    self.cameras += [GLTF2_0PerspectiveCamera(camera["aspectRatio"],camera["yfov"],camera["znear"],far)]
+                elif typeName == "orthographic":
+                    camera = camera["orthographic"]
+                    far = DictInValue(self.header,"cameras")
+                    if far == None:
+                        far = 1000.0
+                    self.cameras += [GLTF2_0OrthographicCamera(camera["xmag"],camera["ymag"],camera["znear"],far)]
+                else:
+                    raise Exception
     #プリミティブの情報を取得
     def ParseMeshes(self):
         self.meshHeaders = []
         for mesh in self.header["meshes"]:
-            self.meshHeaders += [Gltf2_0MeshHeader(mesh)]
+            self.meshHeaders += [GLTF2_0MeshHeader(mesh)]
 
     #bufferViews,accessorを利用して、バイナリデータのパース
     #sparseに対応する必要あり
@@ -187,7 +227,9 @@ class Gltf2_0Loader:
             offset += bufferView["byteOffset"]
             count = accessor["count"]
             bufferData = struct.unpack_from(singleFormat*count,self.buffers[bufferIndex].rawBuffer,offset)
-            self.data += [gltf2_0BufferData(bufferData,count,accessor["min"],accessor["max"])]
+            self.data += [GLTF2_0BufferData(bufferData,count,accessor["min"],accessor["max"])]
+        #生のデータには用がなくなったので削除
+        self.buffers.clear()
     def CreateDB(self):
         #インデックス
         self.dataBase = []
@@ -207,12 +249,11 @@ class Gltf2_0Loader:
                 else:
                     self.dataBaseDict[attribute] = [pair[1]]
                 self.dataBase += [pair]
-            pass
 
 #TODO : レンダラ構築
             
 def Initialize(parent):
-    loader = Gltf2_0Loader()
+    loader = GLTF2_0Loader()
     loader.Load("testModel.gltf")
     loader.Load("C:/Users/black/Desktop/2.0/2CylinderEngine/glTF/2CylinderEngine.gltf")
     loader.Load("C:/Users/black/Desktop/2.0/AlphaBlendModeTest/glTF/AlphaBlendModeTest.gltf")
